4a1fe699eab897b3408e6b1ed443cbb164829f9a
Afrah Fathima M <157614471+Afrahfm@users.noreply.github.com>
2025-09-30 11:43:08 +0530

    Delete springapp directory

--- springapp/src/main/java/com/examly/springapp/repository/ItineraryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.examly.springapp.repository;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.examly.springapp.model.ItineraryEvent;
-
-public interface ItineraryRepository extends JpaRepository<ItineraryEvent, Long> {
-    List<ItineraryEvent> findByTripNameIgnoreCase(String tripName);
-}
